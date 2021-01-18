340931226d1f1b0f20d693362d27e7122bba48de
ederivc <ederivc@gmail.com>
2021-01-18 16:27:01 -0600

    Validate thread status

--- sortingvis/validation.py
+++ sortingvis/validation.py
@@ -10,19 +10,19 @@ def validate_canva(
     if GUI.obj.start_drawing1.start == False:
         draw_info(
             GUI.obj.info_data_1.value[0], GUI.obj.info_data_1.value[1], 
-            canvas_1, GUI.obj.list_1.value
+            canvas_1, GUI.obj.list_1.value, GUI.obj.is_sorting_1.status
         )
 
     if GUI.obj.start_drawing2.start == False:
         draw_info(
             GUI.obj.info_data_2.value[0], GUI.obj.info_data_2.value[1], 
-            canvas_2, GUI.obj.list_2.value
+            canvas_2, GUI.obj.list_2.value, GUI.obj.is_sorting_2.status
         )
         
     if GUI.obj.start_drawing3.start == False:
         draw_info(
             GUI.obj.info_data_3.value[0], GUI.obj.info_data_3.value[1], 
-            canvas_3, GUI.obj.list_3.value
+            canvas_3, GUI.obj.list_3.value, GUI.obj.is_sorting_3.status
         )
 
 def validate_animation(
@@ -36,6 +36,7 @@ def validate_animation(
             GUI.obj.list_1.value, time_list[0]
         )
         GUI.obj.start_drawing1.start = True
+        GUI.obj.is_sorting_1.status = "waiting"
 
     if GUI.obj.start_drawing2.start == "animate":
         final_animation(
@@ -43,6 +44,7 @@ def validate_animation(
             GUI.obj.list_2.value, time_list[1]
         )
         GUI.obj.start_drawing2.start = True
+        GUI.obj.is_sorting_2.status = "waiting"
             
     if GUI.obj.start_drawing3.start == "animate":
         final_animation(
@@ -50,3 +52,4 @@ def validate_animation(
             GUI.obj.list_3.value, time_list[2]
         )
         GUI.obj.start_drawing3.start = True
+        GUI.obj.is_sorting_3.status = "waiting"
