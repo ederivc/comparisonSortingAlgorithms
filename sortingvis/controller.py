c723e73dbe4bcc6ae824299d228b32b13b09ddc1
ederivc <ederivc@gmail.com>
2021-01-18 17:58:47 -0600

    Validate thread status

--- sortingvis/controller.py
+++ sortingvis/controller.py
@@ -7,7 +7,9 @@ from tkinter import Canvas, Button
 from tkinter import messagebox as mBox
 
 
-def draw_info(comp_value: int, color: list, canva: Canvas, _list: list) -> None:
+def draw_info(
+    comp_value: int, color: list, canva: Canvas, _list: list, sorting: str
+) -> None:
     canva.delete("all")
     x = 25
     y = 60
@@ -17,11 +19,14 @@ def draw_info(comp_value: int, color: list, canva: Canvas, _list: list) -> None:
             x, value[1]+20, anchor="sw", text=value[0], fill="black"
         )
         canva.create_text(
-            400, 580, anchor="sw", text="Comparisons", fill="black"
+            40, 580, anchor="sw", font=("Helvetica", 10), text="Comparisons", fill="black"
         )
         canva.create_text(
-            400, 600, anchor="sw", text=comp_value, fill="black"
+            40, 600, anchor="sw", font=("Helvetica", 10), text=comp_value, fill="black"
         )
+
+        validate_status(canva, sorting)
+
         x += 40
         y += 40
 
@@ -29,10 +34,14 @@ def final_animation(
     comp_value: int, canva: Canvas , _list: list, _time: int
 ) -> None:
     for _ in _list:
-        draw_info(comp_value, ["#d755a6"  for _ in _list], canva, _list)
+        draw_info(comp_value, ["#d755a6"  for _ in _list], canva, _list, "done")
 
-    canva.create_text(20, 580, anchor="sw", text="TIME", fill="black")
-    canva.create_text(20, 600, anchor="sw", text=_time.time_now, fill="black")
+    canva.create_text(
+        400, 580, anchor="sw", font=("Helvetica", 10), text="TIME", fill="black"
+    )
+    canva.create_text(
+        400, 600, anchor="sw", font=("Helvetica", 10), text=_time.time_now, fill="black"
+    )
 
 
 def select_algorithm(
@@ -54,3 +63,23 @@ def select_algorithm(
     sortingvis.GUI.obj.list_3.value = random_list.copy()
 
     sortingvis.threads.start_thread_animation(algorithm1, algorithm2, algorithm3, speed)
+
+
+def validate_status(canva: Canvas, status: str) -> Canvas:
+    if status == "waiting":
+        return canva.create_text(
+            210, 580, anchor="sw", font=("Helvetica", 10), text="Status: Waiting", 
+            fill="black"
+        )
+    
+    if status == "sorting":
+        return canva.create_text(
+            210, 580, anchor="sw", font=("Helvetica", 10), text="Status: Sorting", 
+            fill="black"
+        )
+    
+    if status == "done":
+        return canva.create_text(
+            210, 580, anchor="sw", font=("Helvetica", 10), text="Status: Sorted", 
+            fill="black"
+        )
