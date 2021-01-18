119e6a69297c0ace32324a5074e4184823aaa008
ederivc <ederivc@gmail.com>
2021-01-18 16:28:05 -0600

    Modified draw arguments

--- sortingvis/threads.py
+++ sortingvis/threads.py
@@ -12,17 +12,17 @@ def start_thread(
 
     t1 = threading.Thread(
         target=draw_info, args=(0, ["#ffa500" for x in enumerate(_list)], 
-        canvas_1, _list), daemon=True
+        canvas_1, _list, GUI.obj.is_sorting_1.status), daemon=True
     )
 
     t2 = threading.Thread(
         target=draw_info, args=(0, ["#ffa500" for x in enumerate(_list)],
-        canvas_2, _list), daemon=True
+        canvas_2, _list, GUI.obj.is_sorting_2.status), daemon=True
     )
 
     t3 = threading.Thread(
         target=draw_info, args=(0, ["#ffa500" for x in enumerate(_list)],
-        canvas_3, _list), daemon=True
+        canvas_3, _list, GUI.obj.is_sorting_3.status), daemon=True
     )
 
     t1.start()
@@ -36,17 +36,20 @@ def start_thread_animation(
 
     t1 = threading.Thread(
         target=algorithm_1, args=(speed , GUI.obj.list_1, GUI.obj.start_drawing1,
-        GUI.obj.comparisons_1, GUI.obj.info_data_1, GUI.obj.time_1), daemon=True
+        GUI.obj.comparisons_1, GUI.obj.info_data_1, GUI.obj.time_1, 
+        GUI.obj.is_sorting_1), daemon=True
     )
 
     t2 = threading.Thread(
         target=algorithm_2, args=(speed, GUI.obj.list_2, GUI.obj.start_drawing2, 
-        GUI.obj.comparisons_2, GUI.obj.info_data_2, GUI.obj.time_2), daemon=True
+        GUI.obj.comparisons_2, GUI.obj.info_data_2, GUI.obj.time_2, 
+        GUI.obj.is_sorting_2), daemon=True
     )
 
     t3 = threading.Thread(
         target=algorithm_3, args=(speed, GUI.obj.list_3, GUI.obj.start_drawing3, 
-        GUI.obj.comparisons_3, GUI.obj.info_data_3, GUI.obj.time_3), daemon=True
+        GUI.obj.comparisons_3, GUI.obj.info_data_3, GUI.obj.time_3, 
+        GUI.obj.is_sorting_3), daemon=True
     )
 
     t1.start()
